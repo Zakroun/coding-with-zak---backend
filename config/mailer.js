ca1dc4de4d62e29feeb569c873e47188e9d1760e
zakaryae rouane <rouanezakaria052@gmail.com>
2026-04-04 19:14:52 +0100

    fix: fix mailer code

--- config/mailer.js
+++ config/mailer.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer')
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
 })
 
