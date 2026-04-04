c2e2aebe936f72403305820da235aad5f6135278
zakaryae rouane <rouanezakaria052@gmail.com>
2026-04-04 12:39:39 +0100

    new fix

--- api/contact.js
+++ api/contact.js
@@ -1,9 +1,21 @@
 const { sendContactEmail } = require('../config/mailer')
 
+const ALLOWED_ORIGINS = [
+    'https://codingwiithzak.vercel.app',
+    'https://codingwithzak.com',
+    'http://localhost:5173',
+]
+
 module.exports = async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://codingwithzak.com')
+    const origin = req.headers.origin
+
+    if (ALLOWED_ORIGINS.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin)
+    }
+
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
 
     if (req.method === 'OPTIONS') {
         return res.status(200).end()
